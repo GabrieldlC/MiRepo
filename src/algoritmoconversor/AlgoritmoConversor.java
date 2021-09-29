d7f9a76df3ec2c0109c48a800fd0c5ee991c8bef
Gabriel <gabriel.delacuadra@outlook.com.ar>
2021-09-29 16:28:32 -0300

    nueva rama

--- src/algoritmoconversor/AlgoritmoConversor.java
+++ src/algoritmoconversor/AlgoritmoConversor.java
@@ -4,24 +4,23 @@ public class AlgoritmoConversor {
 
     public static void main(String[] args) {
         Controlador conversor = new Controlador();
-        
+
         conversor.agregarToken("USDT", 190);
         conversor.agregarToken("BTC", 800000);
         conversor.agregarToken("ETH", 550000);
         conversor.agregarToken("ADA", 390);
         conversor.agregarToken("BNB", 64000);
-        
+
         double ars = 50000;
         String token = "BNB";
-        
+
         try {
             double valor = conversor.valorEnCripto(ars, token);
             System.out.println("Su conversion de $" + ars + " a " + token + " es de " + valor);
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
-        
+
     }
 
 }
