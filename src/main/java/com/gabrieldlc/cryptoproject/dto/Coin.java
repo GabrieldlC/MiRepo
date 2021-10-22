77e2a21ff86045b305b18c7b60813b824d918526
GabrieldlC <81653569+GabrieldlC@users.noreply.github.com>
2021-10-22 00:52:23 -0300

    Agrego nuevas conversiones

--- src/main/java/com/gabrieldlc/cryptoproject/dto/Coin.java
+++ src/main/java/com/gabrieldlc/cryptoproject/dto/Coin.java
@@ -1,24 +1,30 @@
 package com.gabrieldlc.cryptoproject.dto;
 
 public class Coin {
+
+    private static final double USD_ARS = 182;
     
     private String tiker;
-    private double price;
+    private double priceArs;
+    private double priceUsd;
     private int decimals;
 
-    public Coin(String tiker, double price, int decimals) {
+    public Coin(String tiker, double priceUsd, int decimals) {
         this.tiker = tiker;
-        this.price = price;
+        this.priceUsd = priceUsd;
         this.decimals = decimals;
+        this.priceArs = priceUsd * USD_ARS;
     }
 
     public String getTiker() {
         return tiker;
     }
 
-    public double getPrice() {
-        return price;
+    public double getPriceArs() {
+        return priceArs;
     }
+
+    public double getPriceUsd() { return  priceUsd; }
 
     public int getDecimals() {
         return this.decimals;
--- src/main/java/com/gabrieldlc/cryptoproject/services/ConversorService.java
+++ src/main/java/com/gabrieldlc/cryptoproject/services/ConversorService.java
@@ -8,45 +8,121 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class ConversorService {
+    private static final String MSG_NEGATIVE_NUM = "El monto ingresado debe ser mayor o igual a 0";
+    private static final String MSG_NO_TOKEN_EXIST = "El token ingresado no existe";
+    private static final String MSG_ERROR = "Error inesperado";
 
     private ArrayList<Coin> tokens;
 
     public ConversorService() {
         tokens = new ArrayList<>();
-        agregarToken("USDT", 190, 2);
-        agregarToken("BTC", 8000000, 8);
-        agregarToken("ETH", 550000, 5);
-        agregarToken("ADA", 390, 3);
-        agregarToken("BNB", 64000, 4);
+        addToken("USDT", 1.01, 2);
+        addToken("BTC", 64000, 8);
+        addToken("ETH", 4200, 5);
+        addToken("ADA", 2.3, 3);
+        addToken("BNB", 490, 4);
     }
 
-    private void agregarToken(String tiker, double price, int decimals) {
+    private void addToken(String tiker, double price, int decimals) {
         tokens.add(new Coin(tiker, price, decimals));
     }
 
-    public double valorEnCripto(double ars, String token) {
+    public double arsToCrypto(double ars, String token) {
         Coin myCoin;
 
         try {
             myCoin = tokens.stream().filter(x -> x.getTiker().equals(token.toUpperCase())).findFirst().get();
 
             if (ars < 0) {
-                throw new IndexOutOfBoundsException("El monto ingresado debe ser mayor o igual a 0");
+                throw new IndexOutOfBoundsException(MSG_NEGATIVE_NUM);
             }
 
-            return limitDecimals(ars / myCoin.getPrice(), myCoin.getDecimals());
+            return limitDecimals(ars / myCoin.getPriceUsd(), myCoin.getDecimals());
         } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("El token ingresado no existe");
+            throw new IllegalArgumentException(MSG_NO_TOKEN_EXIST);
         } catch (Exception e) {
-            throw new RuntimeException("Error inesperado");
+            throw new RuntimeException(MSG_ERROR);
         }
+    }
 
+    public double usdToCrypto(double usd, String token) {
+        Coin myCoin;
+
+        try {
+            myCoin = tokens.stream().filter(x -> x.getTiker().equals(token.toUpperCase())).findFirst().get();
+
+            if (usd < 0) {
+                throw new IndexOutOfBoundsException(MSG_NEGATIVE_NUM);
+            }
+
+            return limitDecimals(usd / myCoin.getPriceUsd(), myCoin.getDecimals());
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException(MSG_NO_TOKEN_EXIST);
+        } catch (Exception e) {
+            throw new RuntimeException(MSG_ERROR);
+        }
     }
 
     public double limitDecimals(double number, int decimals) {
         BigDecimal bd = new BigDecimal(number).setScale(decimals, RoundingMode.HALF_UP);
 
         return bd.doubleValue();
+    }
+
+    public double cryptoToArs(String token, double value) {
+        Coin myCoin;
+
+        try {
+            myCoin = tokens.stream().filter(x -> x.getTiker().equals(token.toUpperCase())).findFirst().get();
+
+            if (value < 0) {
+                throw new IndexOutOfBoundsException(MSG_NEGATIVE_NUM);
+            }
+
+            return limitDecimals(myCoin.getPriceUsd() * value, myCoin.getDecimals());
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException(MSG_NO_TOKEN_EXIST);
+        } catch (Exception e) {
+            throw new RuntimeException(MSG_ERROR);
+        }
+    }
+
+    public double cryptoToUsd(String token, double value) {
+        Coin myCoin;
+
+        try {
+            myCoin = tokens.stream().filter(x -> x.getTiker().equals(token.toUpperCase())).findFirst().get();
+
+            if (value < 0) {
+                throw new IndexOutOfBoundsException(MSG_NEGATIVE_NUM);
+            }
+
+            return limitDecimals(myCoin.getPriceArs() * value, myCoin.getDecimals());
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException(MSG_NO_TOKEN_EXIST);
+        } catch (Exception e) {
+            throw new RuntimeException(MSG_ERROR);
+        }
+    }
+
+    public double cryptoToCrypto(String token1, double value, String token2) {
+        Coin coin1;
+        Coin coin2;
+
+        try {
+            coin1 = tokens.stream().filter(x -> x.getTiker().equals(token1.toUpperCase())).findFirst().get();
+            coin2 = tokens.stream().filter(x -> x.getTiker().equals(token2.toUpperCase())).findFirst().get();
+
+            if (value < 0) {
+                throw new IndexOutOfBoundsException(MSG_NEGATIVE_NUM);
+            }
+
+            return limitDecimals(coin1.getPriceUsd() * value / coin2.getPriceUsd(), coin2.getDecimals());
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException(MSG_NO_TOKEN_EXIST);
+        } catch (Exception e) {
+            throw new RuntimeException(MSG_ERROR);
+        }
     }
 
 }
